faf59b4c9c50635dce7f943af5a0c25ca61839f2
Nick Fox-Gieg <nick@fox-gieg.com>
2023-06-16 16:17:43 -0400

    Update hypercard.glsl

--- processing/GeeBeeCamera/data/hypercard.glsl
+++ processing/GeeBeeCamera/data/hypercard.glsl
@@ -6,11 +6,16 @@ const vec3 lightGray = vec3(0.686, 0.686, 0.686);
 const vec3 darkGray = vec3(0.376, 0.376, 0.376);
 const vec3 black = vec3(0.066, 0.066, 0.066);
 
-const mat3 ditherPattern = mat3(
-    0.0, 0.5, 0.125,
-    0.75, 0.25, 0.875,
-    0.1875, 0.6875, 0.0625
+// https://stackoverflow.com/questions/12422407/monochrome-dithering-in-javascript-bayer-atkinson-floyd-steinberg
+const mat4 ditherPattern = mat4(
+    0.059, 0.529, 0.176, 0.647,
+    0.765, 0.294, 0.882, 0.412,
+    0.235, 0.706, 0.118, 0.588,
+    0.941, 0.471, 0.824, 0.353
 );
+
+const int matrixSize = 4;
+const float finalThreshold = 0.4;
 
 float getLuminance(vec3 col) {
     return dot(col, vec3(0.299, 0.587, 0.114));
@@ -23,8 +28,8 @@ void mainImage(out vec4 fragColor, in vec2 fragCoord) {
     
     float texGray = getLuminance(texColor);
     int paletteIndex = int(texGray * 15.0);
-    int x = int(fragCoord.x) % 3;
-    int y = int(fragCoord.y) % 3;
+    int x = int(fragCoord.x) % matrixSize;
+    int y = int(fragCoord.y) % matrixSize;
     int ditherIndex = int(ditherPattern[x][y] * 15.0);
     paletteIndex = min(paletteIndex + ditherIndex, 15);
     vec3 ditheredColor = vec3(
@@ -36,7 +41,7 @@ void mainImage(out vec4 fragColor, in vec2 fragCoord) {
     float texGray2 = texGray - 0.15 * getLuminance(ditheredColor);
 
     vec3 color;
-    if (texGray2 <= 0.5) {
+    if (texGray2 < finalThreshold) {
         color = black;
     } else {
         color = white;
