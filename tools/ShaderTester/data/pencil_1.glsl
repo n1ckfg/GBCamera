6df7fb7b2f38e55d48c5016cd07662e378d9af90
Nick Fox-Gieg <nick@fox-gieg.com>
2024-07-16 11:39:03 -0400

    Update pencil_1.glsl

--- tools/ShaderTester/data/pencil_1.glsl
+++ tools/ShaderTester/data/pencil_1.glsl
@@ -11,59 +11,59 @@ mat2 rot(float a) {
 }
 
 float gyroid (vec3 seed) { 
-    return dot(sin(seed),cos(seed.yzx)); 
+    return dot(sin(seed), cos(seed.yzx)); 
 }
 
 float noise (vec3 p) {
-    float result = 0., a = .5;
-    for (int i = 0; i < 3; ++i, a/=2.) {
-        result += gyroid(p/a)*a;
+    float result = 0.0, a = 0.5;
+    for (int i = 0; i < 3; i++, a/=2.0) {
+        result += gyroid(p / a) * a;
     }
     return result;
 }
 
 // Dave Hoskins https://www.shadertoy.com/view/4djSRW
 vec4 hash41(float p) {
-    vec4 p4 = fract(vec4(p) * vec4(.1031, .1030, .0973, .1099));
-    p4 += dot(p4, p4.wzxy+33.33);
-    return fract((p4.xxyz+p4.yzzw)*p4.zywx);
+    vec4 p4 = fract(vec4(p) * vec4(0.1031, 0.1030, 0.0973, 0.1099));
+    p4 += dot(p4, p4.wzxy + 33.33);
+    return fract((p4.xxyz + p4.yzzw) * p4.zywx);
 }
 
 vec4 bufferImage(vec4 fragColor, vec2 fragCoord) {
-    vec2 uv = fragCoord/iResolution.xy;
-    vec2 aspect = vec2(iResolution.x/iResolution.y,1);
+    vec2 uv = fragCoord / iResolution.xy;
+    vec2 aspect = vec2(iResolution.x / iResolution.y, 1);
     vec4 frame = texture(tex0, uv);
     
     // timeline
-    float tick = mod(float(time), 200.);
+    float tick = mod(float(time), 200.0);
     
     // init
-    if (tick < 1.) {
-        vec2 p = (2.*fragCoord-iResolution.xy)/iResolution.y;
+    if (tick < 1.0) {
+        vec2 p = (2.0 * fragCoord - iResolution.xy) / iResolution.y;
         frame = vec4(p, length(p), 0);
         
         // extra shape
-        frame += step(frame.z, .9);
-    } else if (tick < 30.) {    // iterate
+        frame += step(frame.z, 0.9);
+    } else if (tick < 30.0) {    // iterate
         // previous coordinates
         vec2 p = frame.xy;
         
         // seed
-        vec4 rng = hash41(tick+(time * 4));
+        vec4 rng = hash41(tick);// + (time * 4));
         
         // rotate
-        p.xy *= rot(rng.z*6.283);
+        p.xy *= rot(rng.z * 6.283);
         
         // translate
-        p.xy += (rng.xy-.5)*2.;
+        p.xy += (rng.xy - 0.5) * 2.0;
         
         // curl
-        vec2 e = vec2(.001,0);
-        vec3 pos = vec3(p/2., length(p)/2.);
-        float x = (noise(pos+e.yxy)-noise(pos-e.yxy))/(2.*e.x);
-        float y = (noise(pos+e.xyy)-noise(pos-e.xyy))/(2.*e.x);
+        vec2 e = vec2(0.001, 0);
+        vec3 pos = vec3(p / 2.0, length(p) / 2.0);
+        float x = (noise(pos + e.yxy) - noise(pos - e.yxy)) / (2.0 * e.x);
+        float y = (noise(pos + e.xyy) - noise(pos - e.xyy)) / (2.0 * e.x);
         vec2 curl = vec2(y, -x);
-        p.xy += curl * .5 / tick; // comment this for sharp breaks
+        p.xy += curl * 0.5 / tick; // comment this for sharp breaks
         
         // keep result if closer
         float dist = length(p);
@@ -81,34 +81,34 @@ void mainImage(out vec4 fragColor, in vec2 fragCoord) {
     vec2 uv = fragCoord/iResolution.xy;
     
     // coordinates distance
-    #define T(u) texture(tex0, uv+u/iResolution.xy).z
-    float z = T(0.);
+    #define T(u) texture(tex0, uv + u / iResolution.xy).z
+    float z = T(0.0);
     
-    float edge = 0.;
-    float ao = 0.;
+    float edge = 0.0;
+    float ao = 0.0;
     
-    const float count = 9.;
-    for (float a = 0.; a < count; ++a) {
+    const float count = 9.0;
+    for (float a = 0.0; a < count; a++) {
         // blue noise scroll https://www.shadertoy.com/view/tlySzR
         ivec2 pp = ivec2(fragCoord);
-        pp = (pp+(int(a))*ivec2(113,127)) & 1023;
-        vec3 blu = bufferImage(fragColor, fragCoord).xyz;
+        pp = (pp + (int(a)) * ivec2(113, 127)) & 1023;
+        vec3 blu = bufferImage(fragColor, pp).xyz;
         
         // edge detection
-        float f = a/count;
-        float aa = 6.283*f;
-        vec2 xy = vec2(cos(aa),sin(aa));
-        edge += abs(z-T(xy));
+        float f = a / count;
+        float aa = 6.283 * f;
+        vec2 xy = vec2(cos(aa), sin(aa));
+        edge += abs(z - T(xy));
         
         // sort of ambient occlusion
-        float r = (1.+blu.z) * 4.;
-        xy = normalize(vec2(blu.xy-.5))*r;
-        ao += abs(z-T(xy))/r;
+        float r = (1.0 + blu.z) * 4.0;
+        xy = normalize(vec2(blu.xy - 0.5)) * r;
+        ao += abs(z - T(xy)) / r;
     }
     
-    vec3 color = vec3((1.-edge) * (1.-ao));
+    vec3 color = vec3((1.0 - edge) * (1.0 - ao));
 
-    fragColor = vec4(color, 1.);
+    fragColor = vec4(color, 1.0);
 }
 
 
